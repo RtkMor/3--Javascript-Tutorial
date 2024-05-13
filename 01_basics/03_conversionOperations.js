8924f3a350b8f7905f7ad6ef7f2e5ce45f2f8e26
RtkMor <rtkmor@gmail.com>
2024-05-13 09:49:55 +0530

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = "35"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "35" => 35
+// "35abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
