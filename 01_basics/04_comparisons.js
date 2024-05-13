703e33f679467bcf59bb923b396f70b33de17063
RtkMor <rtkmor@gmail.com>
2024-05-13 11:29:15 +0530

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+/*
+    > , < , >= , <= , == , === , !=
+*/
+
+console.log(null == 0); // false
+console.log(null > 0); // false
+console.log(null >= 0); // true
+
+console.log("2" == 2); // true (check only the value after conversion)
+console.log("2" === 2); // false (check the value as well as the datatype)
