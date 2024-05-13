4853933383646c7cbb2d6e019a284ced6d1a1c0b
RtkMor <rtkmor@gmail.com>
2024-05-13 20:51:00 +0530

    objects and api

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -16,11 +16,11 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.name);
-console.log(JsUser["full name"]);
-console.log(JsUser["email"]);
-console.log(JsUser[mySymbol]);
-console.log(mySymbol);
+// console.log(JsUser.name);
+// console.log(JsUser["full name"]);
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySymbol]);
+// console.log(mySymbol);
 
 
 JsUser.email = "rtkmor@gmail.com";
@@ -32,4 +32,30 @@ JsUser.greeting = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-JsUser.greeting();
+// JsUser.greeting();
+
+
+// Objects Part-2
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 4, c: 5 };
+const obj3 = { d: 6, e: 7};
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3);        // old way to do
+const obj4 = {...obj1, ...obj2, ...obj3};                   // common way to do is by spread operator
+
+console.log(obj4);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(obj4 === obj1);
+// Expected output: true
+
+
+const course = {
+    courseName: "Javascript Tutorials",
+    price: "999",
+    courseInstructor: "Ritik"
+}
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
