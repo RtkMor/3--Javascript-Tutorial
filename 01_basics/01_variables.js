15d8324367dcabbf0b886578c75be40fba4c3245
RtkMor <rtkmor@gmail.com>
2024-05-13 09:34:54 +0530

    commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,17 @@
-console.log("Ritik");
-console.log("Mor");
+const accountId = 17072001
+let accountEmail = "rtkmor@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hisar"
+let accountState;
+
+// accountId = 17072007 (Not allowed, we have declared it using const)
+
+accountEmail = "alconime@gmail.com"
+accountPassword = "12121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* Prefer not to use var 
+because of issue in block and functional scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(1+2) // We are using NodeJS not the Browser
+
+let name = "Ritik"
+let age = 22
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// underfined => value is not defined yet
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
